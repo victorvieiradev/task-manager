a3f804f6c6765badd07b877d14d15dd7ba5b7fd4
Victor Vieira <97565198+victorvieiradev@users.noreply.github.com>
2024-03-02 20:17:52 -0300

    projeto inicial

---
+++ src/test/kotlin/br/com/victor/taskmanager/TaskManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.victor.taskmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
